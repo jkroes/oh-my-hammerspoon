c264a3b2e3b6d1d2805bdf90701c4fd42d932788
Justin Kroes <jkroes14@ucsbalum.com>
2017-06-05 16:05:15 -0700

    Add app watcher to launch_apps plugin
    New app watcher calls a function that discriminates between apps
    launching and focusing via launch_apps.lua.
    
    Focusing brings all windows of the focused app to the front. Launching
    doesn't yet trigger any actions.

--- plugins/windows/launch_apps.lua
+++ plugins/windows/launch_apps.lua
@@ -15,11 +15,25 @@ function winmod.init()
   local hyper = omh.modes[1]
   local parent = omh.modes[2]
 
+  local function applicationWatcher(appName, eventType, appObject)
+    if eventType == hs.application.watcher.launched then
+      --print(2+2) -- example to demonstrate this works
+      --Need to implement: appObject:window move to location
+      -- if appName == "iTerm" then
+      --   appObject:focusedWindow.
+    elseif eventType == hs.application.watcher.activated then
+      appObject:selectMenuItem({"Window", "Bring All to Front"})
+    end
+  end
+  local appWatcher = hs.application.watcher.new(applicationWatcher)
+  appWatcher:start()
+
   hs.fnutils.ieach(c,
   function(element)
     parent:bind({}, element[1],
     function()
-      hs.application.launchOrFocus(element[2])
+      local appName = element[2]
+      hs.application.launchOrFocus(appName)
       hyper.watch = nil -- must come before exit()!!!
       parent:exit()
     end)
