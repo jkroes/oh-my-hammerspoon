307055ee26c49721a1ba1dba1c9793e4b43328e6
Justin Kroes <jkroes14@ucsbalum.com>
2017-06-07 05:16:20 -0700

    Add actions to app launch callback function
    Experiment with functionality of eventtap and delayed timers.
    Delayed timers are a crude workaround for waiting til a mode is entered
    before emitting a new key. Plan to update tomorrow.

--- plugins/windows/launch_apps.lua
+++ plugins/windows/launch_apps.lua
@@ -18,12 +18,28 @@ function mod.init()
   omh.bindMode2Mode(hyper, modalKey, modalPhrase)
   local launchMode = omh.modes[modalPhrase]
 
+  local function moveWindow(corner, delay1, delay2, delay3)
+    local timer1 = hs.timer.delayed.new(delay1, function()
+      hs.eventtap.keyStroke({},"f13")
+    end)
+    local timer2 = hs.timer.delayed.new(delay2, function()
+      hs.eventtap.keyStroke({},"q")
+    end)
+    local timer3 = hs.timer.delayed.new(delay3, function()
+      hs.eventtap.keyStroke({},corner)
+    end)
+    timer1:start()
+    timer2:start()
+    timer3:start()
+  end
+
   local function applicationWatcher(appName, eventType, appObject)
     if eventType == hs.application.watcher.launched then
-      --print(2+2) -- example to demonstrate this works
-      --Need to implement: appObject:window move to location
-      -- if appName == "iTerm" then
-      --   appObject:focusedWindow.
+      if appName == "iTerm2" then
+        moveWindow("j",0.2,0.5,0.8)
+      elseif appName == "Dash" then
+        moveWindow("u",2,3,4)
+      end
     elseif eventType == hs.application.watcher.activated then
       appObject:selectMenuItem({"Window", "Bring All to Front"})
     end
